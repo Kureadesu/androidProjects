edf9d76e7ccd44f2105758abdc8cc20ec8c852a9
Kureadesu <balaba.claire2003@gmail.com>
2024-10-23 21:32:20 +0800

    done search page

--- kopi/app/src/main/java/com/app/kopi/SearchPage.java
+++ kopi/app/src/main/java/com/app/kopi/SearchPage.java
@@ -1,6 +1,9 @@
 package com.app.kopi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,17 @@ public class SearchPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button back_btn = (Button) findViewById(R.id.backBtn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backBtn(v);
+            }
+        });
+    }
+
+    public void backBtn(View v) {
+        startActivity(new Intent(SearchPage.this, HomePage.class));
+        finish();
     }
 }
